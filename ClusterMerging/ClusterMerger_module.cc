d62e955a6d30c86ff38c7a93263411fe1babccd4
David Caratelli <devish.c@gmail.com>
2018-02-13 08:38:04 -0600

    cluster creation ok, need associations now... --DC

--- ClusterMerging/ClusterMerger_module.cc
+++ ClusterMerging/ClusterMerger_module.cc
@@ -46,8 +46,15 @@ public:
 
 private:
 
+  /**
+     @brief given a cluster::Cluster fill a recob::Cluster
+     @input Cluster   -> reference to cluster to be created
+     @input CMCluster -> where cluster variables are currently stored
+     @input n         -> index of current cluster
+   */
   void FillClusterProperties(::recob::Cluster& cluster,
-			     const ::cluster::Cluster& CMCluster);
+			     const ::cluster::Cluster& CMCluster,
+			     const size_t& n);
 
   // Declare member data here.
 
@@ -102,7 +109,6 @@ void ClusterMerger::produce(art::Event & e)
   // load vertices
   art::Handle<std::vector<recob::Vertex> > vtx_h;
   e.getByLabel(fVertexProducer,vtx_h);
-  
 
   // create cluster::Clusters
   std::vector<::cluster::Cluster> event_clusters;
@@ -119,9 +125,11 @@ void ClusterMerger::produce(art::Event & e)
   for (auto const& out_clus : out_clusters) {
 
     ::recob::Cluster outclus;
-    FillClusterProperties(outclus,out_clus);
+    FillClusterProperties(outclus,out_clus,Cluster_v->size());
     
     Cluster_v->emplace_back(outclus);
+
+    // create association to hits
     
   }// for all output clsters
   
@@ -141,32 +149,25 @@ void ClusterMerger::endJob()
 }
 
 void ClusterMerger::FillClusterProperties(::recob::Cluster& cluster,
-					  const ::cluster::Cluster& CMCluster) {
+					  const ::cluster::Cluster& CMCluster,
+					  const size_t& n) {
   
+  auto const* geom = ::lar::providerFrom<geo::Geometry>();
+
   float startW = CMCluster._start_pt._w;
   float startT = CMCluster._start_pt._t;
   
   float endW   = CMCluster._end_pt._w;
   float endT   = CMCluster._end_pt._t;
   
-  cluster.set_planeID(geo::PlaneID(0,0,CMCluster._plane));
-  
-  cluster.set_start_wire( int(startW / _wire2cm), 0.5);
-  cluster.set_start_tick( int(startT / _time2cm), 0.5);
-  
-  cluster.set_end_wire( int(endW / _wire2cm), 0.5);
-  cluster.set_end_tick( int(endT / _time2cm), 0.5);
-  
-  cluster.set_start_angle( CMCluster._angle );
-  cluster.set_summedADC( CMCluster._sum_charge, 0., 0.);
-  cluster.set_n_hits( CMCluster.size() );
+  auto planeid = geo::PlaneID(0,0,CMCluster._plane);
 
   recob::Cluster clus(startW, 0., startT, 0., 0., CMCluster._angle, 0., 
 		      endW,   0., endT,   0., 0., 0., 0., 
 		      CMCluster._sum_charge, 0., CMCluster._sum_charge, 0., 
 		      CMCluster.size(), 0., 0., n,
-		      hits->at(clus_idx_v[0]).View(),
-		      geo::PlaneID(0,0,CMCluster._plane));
+		      geom->View(planeid),
+		      planeid);
   
 }
 
